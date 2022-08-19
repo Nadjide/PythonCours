686beb1c16a6341977e425e67cab9643ced7e0be
Nadjide.O <106732960+Nadjide@users.noreply.github.com>
2022-08-19 15:27:19 +0200

    variablecours

---
+++ main.py
@@ -0,0 +1,21 @@
+#Premier pas de la formation python
+
+"""Nombre = 8.78
+NomDePersonne = "Gilbert"
+
+print("{} à fait {} CU/H !".format(NomDePersonne,Nombre))"""
+
+#2nd Partie
+
+"""CodeTA = input("Quel est votre codeTA ? ")
+
+print("Bienvenue",CodeTA)"""
+
+#3e partie
+
+"""prixHT = input("Entrez un prix HT :")
+prixHT = int(prixHT)
+
+prixTTC = prixHT + (prixHT*20/100)
+
+print("Print TTC =", prixTTC)"""
